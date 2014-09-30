49691bb8ce2ab9e4d909b82a977ba50af28f738e
daniel.jampen@gmail.com <daniel.jampen@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-09-30 13:01:11 +0000

    added Helper class implemented Student.cs

---
+++ trunk/SecureExam/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureExam
+{
+    class Helper
+    {
+        private static Random random = new Random();
+
+        public static string GenerateRandomAlphaNumericChars( int amountOfChars )
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
+                        "abcdefghijklmnopqerstuvwxyz" +
+                        "0123456789";
+            return new string(Enumerable.Repeat(chars, amountOfChars).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
--- trunk/SecureExam/Student.cs
+++ trunk/SecureExam/Student.cs
@@ -8,5 +8,31 @@ namespace SecureExam
 {
     class Student
     {
+        // members
+        private string StudentName;
+        private string StudentID;
+
+        // setter n getter
+        public string studentName 
+        {
+            get { return StudentName; }
+            set { StudentName = value; }
+        }
+        public string studentID
+        {
+            get { return StudentID; }
+            set { StudentID = value; }
+        }
+
+        // methods
+        public string generateStudentSecret()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(studentName);
+            sb.Append(studentID);
+            sb.Append(Helper.GenerateRandomAlphaNumericChars(BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret));
+            return sb.ToString();
+        }
+        
     }
 }
