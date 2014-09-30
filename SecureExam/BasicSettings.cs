6330591174188f4a936f86861df6bbc4ec26d7f6
daniel.jampen@gmail.com <daniel.jampen@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-09-30 12:44:29 +0000

    implemented BasicSettings

--- SecureExam/BasicSettings.cs
+++ SecureExam/BasicSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,71 @@ namespace SecureExam
 {
     class BasicSettings
     {
+        //members
+        private static BasicSettings instance;
+        private string professor;
+        private string subject;
+        private string examTitle;
+        private Path formularFilePath;
+        private Path studentFilePath;
+        private int numberOfRandomCharsInStudentSecret;
+        private int aesKeyLength;
+        private int pbkdf2Iterations;
+
+        // getter n setter
+        public string Professor
+        {
+            get { return professor; }
+            set { professor = value; }
+        }
+        public string Subject 
+        { 
+            get { return subject; }; 
+            set { subject = value; }; 
+        }
+        public string ExamTitle 
+        { 
+            get { return examTitle; }; 
+            set { examTitle = value; }; 
+        }
+        public Path FormularFilePath 
+        { 
+            get { return formularFilePath; }; 
+            set { formularFilePath = value; }; 
+        }
+        public Path StudentFilePath 
+        { 
+            get { return studentFilePath; }; 
+            set { studentFilePath = value; }; 
+        }
+        public int NumberOfRandomCharsInStudentSecret 
+        { 
+            get { return numberOfRandomCharsInStudentSecret; }; 
+            set { numberOfRandomCharsInStudentSecret = value; }; 
+        }
+        public int AESKeyLength 
+        { 
+            get { return aesKeyLength; }; 
+            set { aesKeyLength = value; }; 
+        }
+        public int PBKDF2Iterations 
+        { 
+            get { return pbkdf2Iterations; }; 
+            set { pbkdf2Iterations = value; }; 
+        }
+        
+
+        // singleton
+        private BasicSettings() { }
+
+        // methods
+        public static BasicSettings getInstance()
+        {
+            if( instance == null )
+            {
+                instance = new BasicSettings();
+            }
+            return instance;
+        }
     }
 }
