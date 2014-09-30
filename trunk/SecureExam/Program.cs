0abe89bdc734cb1781a3205ee7ebf59b469a49ca
simonlukes92@gmail.com <simonlukes92@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-09-30 11:46:24 +0000

    Projekt erstellt

---
+++ trunk/SecureExam/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // word und xml file angeben
+            foreach(string token in args){
+                if (token.EndsWith(".xml"))
+                {
+
+                }
+                if (token.EndsWith(".doc") || token.EndsWith(".docx"))
+                {
+                    //
+                    //FileInfo f = new FileInfo(token);
+                    //StreamReader sr = new StreamReader(token);
+                    //String line = sr.ReadLine();
+                    //while(line != ){
+
+
+                    }
+                    
+                }
+            }
+
+        }
+    }
+}
