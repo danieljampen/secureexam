04e23c18c93b5ef2dc4f1218c34057b4696b3ce2
daniel.jampen@gmail.com <daniel.jampen@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-11-17 11:20:11 +0000

    fixfux

--- trunk/SecureExam/bin/Debug/jsremake/secureExam.js
+++ trunk/SecureExam/bin/Debug/jsremake/secureExam.js
@@ -773,7 +773,7 @@ SecureExam.Exam = function (htmlInfo) {
         that.removeAllEventListeners();
         that.InternetAccess.stop();
         that.SecureTime.stop();
-        that.HTMLInfo.DivQuestions.innerHTML = '<p class="questionText center">Prüfung beendet!</p>'
+        that.HTMLInfo.DivQuestions.innerHTML = '<p class="questionText center">Prüfung beendet!</p>';
     }
 
     this.removeAllEventListeners = function () {
