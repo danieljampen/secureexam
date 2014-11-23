13176626ccac2407a0bf1caa0e003fa2dd626d55
daniel.jampen@gmail.com <daniel.jampen@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-11-23 16:02:28 +0000

    Added XMLStudentParser Test

---
+++ UnitTestProject/XMLStudentPaserTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SecureExam
+{
+    [TestClass]
+    public class XMLStudentPaserTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void parseWrongFileNameTest()
+        {
+            XMLStudentParser parser = new XMLStudentParser();
+            parser.parse("invalid.xml");
+        }
+
+        [TestMethod]
+        public void parseTestFile()
+        {
+            XMLStudentParser parser = new XMLStudentParser();
+            LinkedList<Participant> list = parser.parse("studentsTest.xml");
+            BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret = 10;
+
+
+            foreach (Participant p in list)
+            {
+                if( p.GetType() == typeof(Professor) ){
+                    Assert.AreEqual("rege",((Professor)p).name);
+                    Assert.AreEqual(10, ((Professor)p).secret.Length);
+                }
+                else if (p.GetType() == typeof(Student))
+                {
+                    Assert.AreEqual("Daniel", ((Student)p).studentPreName);
+                    Assert.AreEqual("Jampen", ((Student)p).studentSurName);
+                    Assert.AreEqual("S12198320", ((Student)p).studentID);
+                    Assert.AreEqual(10, ((Student)p).secret.Length);
+                }
+            }
+        }
+        
+    }
+}
