481a2b3b5fe721d9cc8d3c3a88f28f896052cde9
simonlukes92@gmail.com <simonlukes92@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-11-23 23:59:08 +0000

    schreibfehler korrigiert

---
+++ UnitTestProject/XMLStudentParserTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.QualityTools.Testing.Fakes;
+
+namespace SecureExam
+{
+    [TestClass]
+    public class XMLStudentParserTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void parseWrongFileNameTest()
+        {
+            XMLStudentParser parser = new XMLStudentParser();
+            parser.parse("invalid.xml");
+        }
+
+        [TestMethod]
+        public void parseTestFile()
+        {
+            XMLStudentParser parser = new XMLStudentParser();
+            LinkedList<Participant> list = parser.parse("studentsTest.xml");
+            BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret = 10;
+
+            foreach (Participant p in list)
+            {
+                if( p.GetType() == typeof(Professor) )
+                {
+                    Professor professor = (Professor)p;
+                    Assert.AreEqual("Karl", professor.preName);
+                    Assert.AreEqual("Rege", professor.surName);
+                    Assert.AreEqual(18, professor.ParticipantSecret.Length);
+                }
+                else if (p.GetType() == typeof(Student))
+                {
+                    Assert.AreEqual("Daniel", ((Student)p).studentPreName);
+                    Assert.AreEqual("Jampen", ((Student)p).studentSurName);
+                    Assert.AreEqual("S12198320", ((Student)p).studentID);
+                    Assert.AreEqual(31, ((Student)p).ParticipantSecret.Length);
+                }
+            }
+        }
+    }
+}
