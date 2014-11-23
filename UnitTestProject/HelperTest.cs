ee45bb506206498da753bbe82cf3a57da0a25dfe
daniel.jampen@gmail.com <daniel.jampen@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-11-23 17:02:53 +0000

    started helpertest

---
+++ UnitTestProject/HelperTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecureExam
+{
+    [TestClass]
+    public class HelperTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ByteArrayToHexArrayTestNull()
+        {
+            Helper.ByteArrayToHexString(null);
+        }
+        
+        [TestMethod]
+        public void ByteArrayToHexArrayTest()
+        {
+            Byte[] array = {0x20,0x20,0x25};
+            Assert.AreEqual("202025",Helper.ByteArrayToHexString(array));
+        }
+    }
+}
