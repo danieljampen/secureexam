89a3033a7fd97f521463ce114715921ef8666f98
daniel.jampen@gmail.com <daniel.jampen@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-11-24 11:47:30 +0000

    fix fix.. fake basicsettings :)

--- UnitTestProject/ProfessorTest.cs
+++ UnitTestProject/ProfessorTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
 
 namespace SecureExam
 {
@@ -17,34 +18,43 @@ namespace SecureExam
         [TestMethod]
         public void testSecretLength()
         {
-            BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret = 10;
+            using (ShimsContext.Create())
+            {
+                SecureExam.Fakes.ShimBasicSettings.AllInstances.NumberOfRandomCharsInStudentSecretGet = (a) => { return 10; };
 
-            String preName = "Karl";
-            String surName = "Rege";
-            Professor prof = new Professor(preName, surName);
-            Assert.AreEqual(18, prof.ParticipantSecret.Length);
+                String preName = "Karl";
+                String surName = "Rege";
+                Professor prof = new Professor(preName, surName);
+                Assert.AreEqual(18, prof.ParticipantSecret.Length);
+            }
         }
 
         [TestMethod]
         public void testProfessorParticipantSecretLength()
         {
-            BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret = 10;
+            using (ShimsContext.Create())
+            {
+                SecureExam.Fakes.ShimBasicSettings.AllInstances.NumberOfRandomCharsInStudentSecretGet = (a) => { return 10; };
 
-            String preName = "Karl";
-            String surName = "Rege";
-            Professor prof = new Professor(preName, surName);
-            Assert.AreEqual(surName.Length + preName.Length + BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret, prof.ParticipantSecret.Length);
+                String preName = "Karl";
+                String surName = "Rege";
+                Professor prof = new Professor(preName, surName);
+                Assert.AreEqual(surName.Length + preName.Length + BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret, prof.ParticipantSecret.Length);
+            }
         }
 
         [TestMethod]
         public void testProfessorSecretIsStatic()
         {
-            BasicSettings.getInstance().NumberOfRandomCharsInStudentSecret = 10;
-            
-            String preName = "Karl";
-            String surName = "Rege";
-            Professor prof = new Professor(preName, surName);
-            Assert.AreEqual(prof.ParticipantSecret, prof.ParticipantSecret);
+            using (ShimsContext.Create())
+            {
+                SecureExam.Fakes.ShimBasicSettings.AllInstances.NumberOfRandomCharsInStudentSecretGet = (a) => { return 10; };
+
+                String preName = "Karl";
+                String surName = "Rege";
+                Professor prof = new Professor(preName, surName);
+                Assert.AreEqual(prof.ParticipantSecret, prof.ParticipantSecret);
+            }
         }
     }
 }
