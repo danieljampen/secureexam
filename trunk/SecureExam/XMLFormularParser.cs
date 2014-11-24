fde25e20472f0f61e840c532e0669fa38c6f61fd
simonlukes92@gmail.com <simonlukes92@gmail.com@e52ae35d-9486-ed34-bfa3-0d8c6fc7926b>
2014-11-24 07:21:42 +0000

    try catch handling

--- trunk/SecureExam/XMLFormularParser.cs
+++ trunk/SecureExam/XMLFormularParser.cs
@@ -17,78 +17,71 @@ namespace SecureExam
             LinkedList<Question> questions = new LinkedList<Question>();
 
             //Create an instance of the XmlTextReader and call Read method to read the file
-            try
-            {
-                this.xmlDoc = new XmlDocument();
-                this.xmlDoc.Load(streamReader);
+            this.xmlDoc = new XmlDocument();
+            this.xmlDoc.Load(streamReader);
 
-                //question
-                XmlNodeList questionlist = this.xmlDoc.GetElementsByTagName("question");
-                for (int i = 0; i < questionlist.Count; i++)
+            //question
+            XmlNodeList questionlist = this.xmlDoc.GetElementsByTagName("question");
+            for (int i = 0; i < questionlist.Count; i++)
+            {
+                XmlNodeList questionData = questionlist[i].ChildNodes;
+                Question question = new Question();
+
+                for (int j = 0; j < questionData.Count; j++)
                 {
-                    XmlNodeList questionData = questionlist[i].ChildNodes;
-                    Question question = new Question();
-
-                    for (int j = 0; j < questionData.Count; j++)
+                    switch (questionData[j].Name)
                     {
-                        switch (questionData[j].Name)
-                        {
-                            case "legend":
-                                question.text = questionData[j].InnerText;
-                                break;
-                            case "input":
-                                XmlNode inputElement = questionData[j];
-                                XmlAttributeCollection attributeList = inputElement.Attributes;
+                        case "legend":
+                            question.text = questionData[j].InnerText;
+                            break;
+                        case "input":
+                            XmlNode inputElement = questionData[j];
+                            XmlAttributeCollection attributeList = inputElement.Attributes;
 
-                                Answer answer = new Answer();
-                                foreach (XmlAttribute attribute in attributeList)
+                            Answer answer = new Answer();
+                            foreach (XmlAttribute attribute in attributeList)
+                            {
+                                switch (attribute.Name)
                                 {
-                                    switch (attribute.Name)
-                                    {
-                                        case "type":
-                                            if (attribute.Value == "checkbox")
+                                    case "type":
+                                        if (attribute.Value == "checkbox")
+                                        {
+                                            question.questionType = QuestionType.CHECK_BOX;
+                                        }
+                                        else if (attribute.Value == "text")
+                                        {
+                                            question.questionType = QuestionType.TEXT_BOX;
+                                        }
+                                        break;
+                                    case "value":
+                                        answer.text = attribute.Value;
+                                        break;
+                                    case "isCorrect":
+                                        if (question.questionType == QuestionType.CHECK_BOX)// makes no sense with type textbox
+                                        {
+                                            if (attribute.Value == "true")
                                             {
-                                                question.questionType = QuestionType.CHECK_BOX;
+                                                answer.isCorrect = true;
                                             }
-                                            else if (attribute.Value == "text")
+                                            else if (attribute.Value == "false")
                                             {
-                                                question.questionType = QuestionType.TEXT_BOX;
+                                                answer.isCorrect = false;
                                             }
-                                            break;
-                                        case "value":
-                                            answer.text = attribute.Value;
-                                            break;
-                                        case "isCorrect":
-                                            if (question.questionType == QuestionType.CHECK_BOX)// makes no sense with type textbox
-                                            {
-                                                if (attribute.Value == "true")
-                                                {
-                                                    answer.isCorrect = true;
-                                                }
-                                                else if (attribute.Value == "false")
-                                                {
-                                                    answer.isCorrect = false;
-                                                }
-                                            }
-                                            break;
-                                        case "placeholder":
-                                            if (question.answers.Count == 0)// If a answer already exists, a placeholder is not used
-                                            {
-                                                answer.placeHolder = attribute.Value;
-                                            }
-                                            break;
-                                    }
+                                        }
+                                        break;
+                                    case "placeholder":
+                                        if (question.answers.Count == 0)// If a answer already exists, a placeholder is not used
+                                        {
+                                            answer.placeHolder = attribute.Value;
+                                        }
+                                        break;
                                 }
-                                question.answers.Add(answer);
-                                break;
-                        }
+                            }
+                            question.answers.Add(answer);
+                            break;
                     }
-                    questions.AddLast(question);
                 }
-            }
-            catch (DirectoryNotFoundException e)
-            {
-                throw new NotImplementedException(e.ToString());
+                questions.AddLast(question);
             }
             return questions;
         }
